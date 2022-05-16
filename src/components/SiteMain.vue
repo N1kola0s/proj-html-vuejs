546052842e507ea77f930b491282b4f4189ca33b
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-05-16 10:11:38 +0200

    add elements in banner_courses_category section

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,9 +1,16 @@
 <template>
   <main id="site_main">
 
-    <section class="banner_courses_category d-flex justify-content-center align-items-center">
+    <section class="banner_courses_category">
+      
+      <ul class="d-flex justify-content-center align-items-center">
+        <li><a href="#"><font-awesome-icon icon="fa-solid fa-chart-column" class="icon" />Business</a></li>
+        <li><a href="#"><font-awesome-icon icon="fa-solid fa-palette" class="icon" />Design</a></li>
+        <li><a href="#"><font-awesome-icon icon="fa-solid fa-gear" class="icon" />Development</a></li>
+        <li><a href="#"><font-awesome-icon icon="fa-solid fa-face-smile" class="icon"/>Lifestyle</a></li>
+        <li><a href="#"><font-awesome-icon icon="fa-solid fa-print" class="icon"/>Office Productivity</a></li>
+      </ul>
     
-
     </section>
     <!-- /.banner_courses_category -->
 
@@ -66,6 +73,27 @@ export default {
   main{
     min-height: calc(100vh - 412px);
     background-color:red;
+
+    .banner_courses_category{
+      background-color:$oxford-blue;
+      margin-bottom:1.375rem;
+      ul{
+        padding:1.375rem 0;
+        li{
+          a{
+            color: $white;
+            display:flex;
+            justify-content: center;
+            align-items: center;
+            padding: 0 2.875rem;
+            .icon{
+              padding-right:6px;
+              font-size:20px;
+            }
+          }
+        }
+      }
+    }
     
   }
     
--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 
 /* import specific icons 👇 QUI AGGIUNGI LE ICONE che vuoi usare in camelCase */
-import { faUserSecret, faMagnifyingGlass, faChevronDown, faBars, faBullhorn, faBriefcase } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret, faMagnifyingGlass, faChevronDown, faBars, faBullhorn, faBriefcase, faChartColumn, faPalette, faGear, faFaceSmile, faPrint } from '@fortawesome/free-solid-svg-icons'
 import { faFacebook, faTwitter, faInstagram, faLinkedin, faGooglePlus, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import { faUser, faBookmark } from '@fortawesome/free-regular-svg-icons'
 
@@ -14,7 +14,7 @@ import { faUser, faBookmark } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* add icons to the library 👇 QUI AGGIUNGI IL NOME DELL'ICONA in camelCase*/
-library.add(faUserSecret, faMagnifyingGlass, faChevronDown, faFacebook, faTwitter, faInstagram, faLinkedin, faGooglePlus, faYoutube, faBars, faUser, faBookmark, faBullhorn, faBriefcase)
+library.add(faUserSecret, faMagnifyingGlass, faChevronDown, faFacebook, faTwitter, faInstagram, faLinkedin, faGooglePlus, faYoutube, faBars, faUser, faBookmark, faBullhorn, faBriefcase, faChartColumn, faPalette, faGear, faFaceSmile, faPrint)
 
 /* add font awesome icon component */
 Vue.component('font-awesome-icon', FontAwesomeIcon)
