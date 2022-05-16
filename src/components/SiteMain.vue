398acd545351baf59bcbe9ff83700aeaa1e0f190
N1kola0s <97747012+N1kola0s@users.noreply.github.com>
2022-05-16 14:47:12 +0200

    add cards in dev_courses section

--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -46,13 +46,18 @@
         <h2>Popoular Develompent Courses</h2>
         <div class="row row-cols-6">
           <div class="col">
-            <div class="card rounded-0 border-0" style="width: 18rem;">
-              <img src="../assets/img/895786_7b4b_2-272x161.jpg" class="card-img-top border-0" alt="image_tablet">
+            <div class="card rounded-0 border-0">
+              <img src="../assets/img/895786_7b4b_2-272x161.jpg" class="card-img-top border-0 rounded-0" alt="image_tablet">
               <div class="card-body">
-                <div>Development</div>
+                <div Class="card_category">Development<font-awesome-icon icon="fa-solid fa-chevron-right" class="icon" /></div>
                 <h5 class="card-title">The Complete iOS 10 & Swift3 Developer Course</h5>
-                <div class="card_info d-flex justify-content-between">
+                <div class="card_info d-flex justify-content-between align-items-center">
                   <div class="vote_average">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/staremptyl.svg" alt="vote_staremtpy" class="star_emtpy">
 
                   </div>
                   <!-- /.vote_average -->
@@ -65,22 +70,176 @@
                   <!-- /.price -->
 
                 </div>
-                <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+                <!-- /card_info -->
+      
                 
               </div>
             </div>
-
           </div>
           <!-- /.col -->
-          <div class="col"></div>
+          <div class="col">
+            <div class="card rounded-0 border-0">
+              <img src="../assets/img/246154_d8b0_3-272x161.jpg" class="card-img-top border-0 rounded-0" alt="image_02">
+              <div class="card-body">
+                <div Class="card_category">Development<font-awesome-icon icon="fa-solid fa-chevron-right" class="icon" /></div>
+                <h5 class="card-title">Web Design for Beginners: Real World Coding in HTML & CSS</h5>
+                <div class="card_info d-flex justify-content-between align-items-center">
+                  <div class="vote_average">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/staremptyl.svg" alt="vote_staremtpy" class="star_emtpy">
+
+                  </div>
+                  <!-- /.vote_average -->
+                  <div class="price d-flex flex-column justify-content-end align-items-end">
+                    <div class="original_price"><del>$129.99</del></div>
+                    <!-- /.original_price -->
+                    <div class="price_discounted"><strong>$65</strong></div>
+                    <!-- /.price_discounted -->
+                  </div>
+                  <!-- /.price -->
+
+                </div>
+                <!-- /card_info -->
+      
+                
+              </div>
+            </div>
+          </div>
           <!-- /.col -->
-          <div class="col"></div>
+
+          <div class="col">
+            <div class="card rounded-0 border-0" >
+              <img src="../assets/img/752950_b773-272x161.jpg" class="card-img-top border-0 rounded-0" alt="image_03">
+              <div class="card-body">
+                <div Class="card_category">Development<font-awesome-icon icon="fa-solid fa-chevron-right" class="icon" /></div>
+                <h5 class="card-title">GitHub Ultimate:Master Git and GitHub - Beginner to Expert</h5>
+                <div class="card_info d-flex justify-content-between align-items-center">
+                  <div class="vote_average">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/staremptyl.svg" alt="vote_staremtpy" class="star_emtpy">
+
+                  </div>
+                  <!-- /.vote_average -->
+                  <div class="price d-flex flex-column justify-content-end align-items-end">
+                    <div class="original_price"><del>$99.99</del></div>
+                    <!-- /.original_price -->
+                    <div class="price_discounted"><strong>$50</strong></div>
+                    <!-- /.price_discounted -->
+                  </div>
+                  <!-- /.price -->
+
+                </div>
+                <!-- /card_info -->
+      
+                
+              </div>
+            </div>
+          </div>
           <!-- /.col -->
-          <div class="col"></div>
+
+          <div class="col">
+            <div class="card rounded-0 border-0" >
+              <img src="../assets/img/1253188_58f7_2-272x161.jpg" class="card-img-top border-0 rounded-0" alt="image_04">
+              <div class="card-body">
+                <div Class="card_category">Development<font-awesome-icon icon="fa-solid fa-chevron-right" class="icon" /></div>
+                <h5 class="card-title">The Complete iOS 11 & Swift Developer Course - Build 20 Apps</h5>
+                <div class="card_info d-flex justify-content-between align-items-center">
+                  <div class="vote_average">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/staremptyl.svg" alt="vote_staremtpy" class="star_emtpy">
+
+                  </div>
+                  <!-- /.vote_average -->
+                  <div class="price d-flex flex-column justify-content-end align-items-end">
+                    <div class="original_price"><del>$199.99</del></div>
+                    <!-- /.original_price -->
+                    <div class="price_discounted"><strong>$100</strong></div>
+                    <!-- /.price_discounted -->
+                  </div>
+                  <!-- /.price -->
+
+                </div>
+                <!-- /card_info -->
+      
+                
+              </div>
+            </div>
+          </div>
           <!-- /.col -->
-          <div class="col"></div>
+
+          <div class="col">
+            <div class="card rounded-0 border-0" >
+              <img src="../assets/img/919872_ed54_6-272x161.jpg" class="card-img-top border-0 rounded-0" alt="image_05">
+              <div class="card-body">
+                <div Class="card_category">Development<font-awesome-icon icon="fa-solid fa-chevron-right" class="icon" /></div>
+                <h5 class="card-title">Android java Masterclass - Become an App Developer</h5>
+                <div class="card_info d-flex justify-content-between align-items-center">
+                  <div class="vote_average">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/staremptyl.svg" alt="vote_staremtpy" class="star_emtpy">
+
+                  </div>
+                  <!-- /.vote_average -->
+                  <div class="price d-flex flex-column justify-content-end align-items-end">
+                    <div class="original_price"><del>$99.99</del></div>
+                    <!-- /.original_price -->
+                    <div class="price_discounted"><strong>$50</strong></div>
+                    <!-- /.price_discounted -->
+                  </div>
+                  <!-- /.price -->
+
+                </div>
+                <!-- /card_info -->
+      
+                
+              </div>
+            </div>
+          </div>
           <!-- /.col -->
-          <div class="col"></div>
+
+          <div class="col">
+            <div class="card rounded-0 border-0" >
+              <img src="../assets/img/951684_9c1a_2-272x161.jpg" class="card-img-top border-0 rounded-0" alt="image_06">
+              <div class="card-body">
+                <div Class="card_category">Development<font-awesome-icon icon="fa-solid fa-chevron-right" class="icon" /></div>
+                <h5 class="card-title">Xamarin Forms: Build Native Cross-platform Apps with C#</h5>
+                <div class="card_info d-flex justify-content-between align-items-center">
+                  <div class="vote_average">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/starfull.svg" alt="vote_starfull">
+                    <img src="../assets/img/staremptyl.svg" alt="vote_staremtpy" class="star_emtpy">
+
+                  </div>
+                  <!-- /.vote_average -->
+                  <div class="price d-flex flex-column justify-content-end align-items-end">
+                    <div class="original_price"><del>$189.99</del></div>
+                    <!-- /.original_price -->
+                    <div class="price_discounted"><strong>$95</strong></div>
+                    <!-- /.price_discounted -->
+                  </div>
+                  <!-- /.price -->
+
+                </div>
+                <!-- /card_info -->
+      
+                
+              </div>
+            </div>
+          </div>
           <!-- /.col -->
         </div>
         <!-- /.row -->
@@ -140,7 +299,35 @@ export default {
 <style scoped lang="scss">
   main{
     min-height: calc(100vh - 412px);
-    background-color:green;
+    h2{
+      font-size: 22px;
+      font-weight: bold;
+      padding-bottom:1.687rem;
+    }
+    /* background-color:green; */
+
+    .card{
+    
+      .card-body{
+        
+        border: 1px solid lightgray;
+        h5{
+          font-size:15px;
+          margin-bottom:15px;
+          padding-bottom:19px;
+          border-bottom: 1px solid lightgray;
+        }
+        .card_category{
+          color:$venus;
+          font-size: 13px;
+          margin-bottom: 10px;
+          .icon{
+            font-size:8px;
+            padding: 0 2px;
+          }
+        }
+      }
+    }
 
     .banner_courses_category{
       background-color:$oxford-blue;
@@ -165,6 +352,9 @@ export default {
 
     .hero{
       background-color: $coral-red;
+        h1{
+          font-weight: bold;
+        }
         .row{
           .col{
             position:relative;
@@ -184,11 +374,25 @@ export default {
               font-size:24px;
             }
           }
+
+          
         }
           
         }
       }
+
+      .dev_courses{
+            padding: 4.375rem 0.625rem;
+          }
+
+
+
+
+
+
     }
+
+    
     
   
     
